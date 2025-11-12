be28d6c66f5c431b61bf36109132aebec61905ea
nasubi <smiley.jinro2525@gmail.com>
2025-11-13 00:45:35 +0900

    selfSet jisso!

--- js/selfset.js
+++ js/selfset.js
@@ -1,8 +1,9 @@
 (() => {
-  const $selfSet = document.getElementById("js-counter");
+  const $counter = document.getElementById("js-counter");
 
   const selfSet = () => {
-    $selfSet.textContent = test;
+    let element = document.getElementById("js-selfset-input").value;
+    $counter.textContent = parseInt(element);
   }
 
   document.getElementById("js-selfset-button").addEventListener("click", selfSet)
